098c29e33f4301ff8fcc638653a4cb9a23790e3d
EC2 Default User <ec2-user@ip-172-31-83-127.ec2.internal>
2020-01-18 15:10:53 +0000

    restoring files to git master

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+if __name__ == '__main__':
+    app.run(host=os.getenv("IP", "0.0.0.0"), 
+    port=int(os.getenv("PORT", 8080)),
+    debug=False)
+    
